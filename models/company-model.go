a4888530b35770636b5c6aabae20ccee0b40cf09
nikitamirzani323 <nikitamirzani323@gmail.com>
2024-03-06 23:55:03 +0700

    update username company tolower

--- models/company-model.go
+++ models/company-model.go
@@ -517,7 +517,7 @@ func Save_companyadmin(admin, idcompany, username, password, name, status, sData
 	flag := false
 
 	if sData == "New" {
-		flag = CheckDB(database_company_local, "adminusername", username)
+		flag = CheckDB(database_company_local, "adminusername", strings.ToLower(username))
 		if !flag {
 			sql_insert := `
 				insert into
